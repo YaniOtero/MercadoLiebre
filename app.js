be8e70e0c7c3fdef3eb2f4a04a3175bdf6abc46e
Yani Otero <yaninaotero91@gmail.com>
2024-02-27 23:38:01 -0300

    render

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let express = require ('express');
 let app = express();
 let path = require ('path');
+let port = process.env.PORT || 3000;
 
-app.listen(3000,()=>console.log('Servidor corriendo'));
+app.listen(port,()=>console.log('Servidor corriendo en el puerto ${port}'));
 
 app.use(express.static('public'));
 app.get('/',(req,res) => {res.sendFile(path.join(__dirname,'/views/home.html'));});
